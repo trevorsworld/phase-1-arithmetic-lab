827192c4cc716dfd2471941ca26f97b34cbd3301
trevors banu <trevorskarithi@gmail.com>
2024-03-21 13:51:03 +0300

    adds phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+let num3 = 64;
+let num4 = 8;
+let mod = num3 % num4;
+console.log(mod);
+let numbers = [1, 6, 3, 8, 12, 20, 5];
+let max = Math.max(...numbers);
+console.log(max);
